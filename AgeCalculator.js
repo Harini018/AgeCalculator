3e9d5e56b29bfd58e6a2fc7359c8b3cea61fa055
Harini <harinis9526@gmail.com>
2024-02-08 20:26:55 +0530

    first commit

---
+++ AgeCalculator.js
@@ -0,0 +1,24 @@
+function age(){
+    var d1=document.querySelector('#date').value;
+    var m1=document.querySelector('#month').value;
+    var y1=document.querySelector('#year').value;
+    var date=new Date();
+    var d2=date.getDate();
+    var m2=1+date.getMonth();
+    var y2=date.getFullYear();
+    var month=[31,28,31,30,31,30,31,31,30,31,30,31];
+    if(d1>d2){
+        d2=d2+month[m2];
+        const d1=document.querySelector('#date');
+        m2=m2-1;
+    }
+    if(m1>m2){
+        m2=m2+12;
+        y2=y2-1;
+    }
+    var d=d2-d1;
+    var m=m2-m1;
+    var y=y2-y1;
+    document.querySelector('#age').innerHTML= `Your Age is ${y} Years ${m} Months ${d} Days`;
+
+}
